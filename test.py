b1e535c1c9bbec1954d04e5dcfd880cbc0b6dd44
Jay-Allemand Maxime <maxime.jay.allemand@hydris-hydrologie.fr>
2023-05-24 16:56:59 +0200

    Modified version of py-gnuplot: modifications allow to plot multi-dataframe in a single plot command. use function load_dataframe to load the dataframe into gnuplot. use function plot_dataframe() to add a dataframe plot to a single plot call. Use plot_dataframe_finalise() to plot and finalise the multi-dataframe plot

---
+++ test.py
@@ -0,0 +1,19 @@
+from pygnuplot import gnuplot
+import numpy as np
+import pandas as pd
+data={"x":np.zeros(10)+1.,"y":np.arange(0,10)}
+data2={"x":np.zeros(10)+1.,"y":np.arange(0,10)*2.}
+df1=pd.DataFrame(data)
+df2=pd.DataFrame(data2)
+
+g=gnuplot.Gnuplot(log=True)
+g.plot_data(df1,'using 1:3 w l')
+
+
+g.load_dataframe(df1,'df1')
+g.load_dataframe(df2,'df2')
+
+g.plot_dataframe('$df1 using 1:3 w l')
+g.plot_dataframe('$df2 using 1:3 w l')
+
+g.plot_dataframe_finalise()
